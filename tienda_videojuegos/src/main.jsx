7d308234cf47d25386f078fa7c0bfa41b4352eaf
Gustavo Marin <wstov@hotmail.com>
2023-10-21 11:50:59 -0600

    Router - React-Router-Dom
    Se crea la estructura para redireccionar los distintos componentes

--- tienda_videojuegos/src/main.jsx
+++ tienda_videojuegos/src/main.jsx
@@ -5,14 +5,21 @@ import './index.css'
 
 import { createBrowserRouter, RouterProvider  } from 'react-router-dom'
 
-const router = createBrowserRouter({
+const router = createBrowserRouter([
+ {
   path:'/',
-  element
-})
+  element:<h1>Inicio</h1>
+ },
+ {
+  path:'/nosotros',
+  element:<h1>Nosotros</h1>
+ }
+
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
