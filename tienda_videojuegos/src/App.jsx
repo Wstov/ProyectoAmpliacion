e2a7906bb626af8807ce0adeac68425399293a22
Gustavo Marin <wstov@hotmail.com>
2023-10-21 11:46:06 -0600

    Inicio del Proyecto con React
    Se crea el proyecto con react y se agregan dependencias como :  "dependencies": {
        "bootstrap": "^5.3.2",
        "react": "^18.2.0",
        "react-dom": "^18.2.0",
        "react-router-dom": "^6.17.0"
      },
      "devDependencies": {
        "@types/bootstrap": "^5.2.8",
        "@types/react": "^18.2.15",
        "@types/react-dom": "^18.2.7",
        "@vitejs/plugin-react": "^4.0.3",
        "eslint": "^8.45.0",
        "eslint-plugin-react": "^7.32.2",
        "eslint-plugin-react-hooks": "^4.6.0",
        "eslint-plugin-react-refresh": "^0.4.3",
        "vite": "^4.4.5"
      }

---
+++ tienda_videojuegos/src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+    </>
+  )
+}
+
+export default App
